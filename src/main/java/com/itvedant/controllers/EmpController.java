d32f143683ffbc76648b5bf966ebe5cde3a90fa3
Mihir Shinde <mihir2403@gmail.com>
2023-01-30 18:08:33 +0530

    initial commit

--- src/main/java/com/itvedant/controllers/EmpController.java
+++ src/main/java/com/itvedant/controllers/EmpController.java
@@ -1,7 +1,8 @@
 package com.itvedant.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,51 +11,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itvedant.models.Employee;
+import com.itvedant.services.Services;
 
 @RestController
 public class EmpController {
 
-	// Using ArrayList as a Resource instead of DB
-	List<Employee> employees = new ArrayList<>();
-
-	public EmpController()
-	{
-		// Added some sample employees..
-		employees.add(new Employee(1, "Mihir", "mihir2403@gmail.com"));
-		employees.add(new Employee(2, "Harry", "harry2403@gmail.com"));
-	}
+	@Autowired
+	Services service;
 
 	// 1. Get all the employees in Response Body
 	@GetMapping("/getemp")
 	public List<Employee> getEmployees()
 	{
-		return this.employees;
+		return this.service.getEmployees();
 	}
 
 	// 2. To Add a new employee
 	@PostMapping("/addemp")
 	public List<Employee> addEmployee(@RequestBody Employee newEmp)
 	{
-		employees.add(newEmp);
-		return this.employees;
+		return this.service.addEmployee(newEmp);
 	}
 
 	// 3. To Update details of a particular Employee
 	@PutMapping("/updateemp")
 	public List<Employee> updateEmployee(@RequestBody Employee newEmp)
 	{
-		for (int i = 0; i < employees.size(); i++)
-		{
-			Employee curEmployee = employees.get(i);
-
-			if (curEmployee.equals(newEmp))
-			{
-				curEmployee.setFirstName(newEmp.getFirstName());
-				curEmployee.setEmail(newEmp.getEmail());
-			}
-		}
-
-		return this.employees;
+		return this.service.updateEmployee(newEmp);
 	}
 
 	// Delete an Employee
@@ -62,15 +45,7 @@ public class EmpController {
 	@DeleteMapping("/deleteemp")
 	public List<Employee> deleteEmployee(@RequestBody Employee delEmp)
 	{
-		for (int i = 0; i < employees.size(); i++)
-		{
-			Employee curEmployee = employees.get(i);
-			if (curEmployee.equals(delEmp))
-			{
-				employees.remove(i);
-			}
-		}
-		return this.employees;
+		return this.service.deleteEmployee(delEmp);
 	}
 
 }
---
+++ src/main/java/com/itvedant/services/Services.java
@@ -0,0 +1,20 @@
+package com.itvedant.services;
+
+import java.util.List;
+
+import com.itvedant.models.Employee;
+
+public interface Services {
+
+	// 1. Get employees
+	public List<Employee> getEmployees();
+
+	// 2. Add employee
+	public List<Employee> addEmployee(Employee newEmp);
+
+	// 3. Update Employee
+	public List<Employee> updateEmployee(Employee newEmp);
+
+	// 4. Delete Employee
+	public List<Employee> deleteEmployee(Employee delEmp);
+}
