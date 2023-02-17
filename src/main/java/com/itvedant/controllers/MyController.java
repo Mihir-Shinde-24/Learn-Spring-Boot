9647f0fdbe1a14813d34d67899002e5890cb2f0c
Mihir Shinde <mihir2403@gmail.com>
2023-02-17 13:08:20 +0530

    cors example added

--- src/main/java/com/itvedant/controllers/MyController.java
+++ src/main/java/com/itvedant/controllers/MyController.java
@@ -1,22 +1,51 @@
 package com.itvedant.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+//@CrossOrigin("*") 					// Any Origin can access any Method if we apply annotation on class level
 public class MyController {
 
-	@GetMapping({"/","/home"})
+	@GetMapping("/myapp")
 	public String homePage()
 	{
 		return "home.html";
 	}
 	
-	@PostMapping("/savedata")
-	public String saveData(String userName)
+//	@CrossOrigin("*") 						// Any Origin can access
+	@CrossOrigin("http://localhost:9090")	// Particular Origin can only Access
+	@PostMapping("/accessresource")
+	@ResponseBody
+	public String accessResource()
 	{
-		System.out.println(userName);
-		return "home.html";
+		return "Hello Resource!";
 	}
+	
+	
+/*
+	Further Steps:
+	 
+	 1. Create Another App 'ConsumerApp' with port 9090 with exact same contents like FirstApp application
+	 
+	 2. Delete the [POST] '/accessresouce' method of 'ConsumerApp', since we are going to 
+	 use this API of 'FirstApp' Application from ConsumerApp application.
+
+
+	3. If we try to access '/accessresource' api from another origin which is 
+	'http://localhost:9090', We will get an Error in browser console..
+	
+	Error:	Access to fetch at 'http://localhost:8080/accessresource' from 
+			origin 'http://localhost:9090' has been blocked by CORS 
+			policy: No 'Access-Control-Allow-Origin' header is present on 
+			the requested resource. If an opaque response serves your needs, 
+			set the request's mode to 'no-cors' to fetch the resource with 
+			CORS disabled.
+	
+	4. So we will add @CrossOrigin("origin") annotation on the '/accessresouce'
+	method, so that different origins will be also able to access the api.
+*/
 }
--- src/main/java/com/itvedant/security/SecurityConfig.java
+++ src/main/java/com/itvedant/security/SecurityConfig.java
@@ -2,36 +2,17 @@ package com.itvedant.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-	
-
-	/* We can disable csrf like this */
-	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception
-//	{
-//		http.csrf().disable();
-//	}
-	 
-	/* But If we disable csrf , the user as well as attacker both will be able to send request*/
-	/* Thus, our requests should always have csrf protection. So we will not disable it */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
-		http
-			.authorizeRequests()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.formLogin();
+		http.csrf().disable();
 	}
-	
-	/*Either we can use above implementation or don't override the method */
-	
-	
 }
