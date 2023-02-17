dd771047f3296ce4c178bd99ec91413b596152c0
Mihir Shinde <mihir2403@gmail.com>
2023-02-17 21:57:18 +0530

    added

--- src/main/java/com/itvedant/controllers/MyController.java
+++ src/main/java/com/itvedant/controllers/MyController.java
@@ -1,27 +1,24 @@
 package com.itvedant.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.itvedant.models.Student;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller // When dealing with frontend, we use @Controller instead of @RestController
 public class MyController {
 
-	@GetMapping({ "/", "/home" })
-	public String home(Model model)
+	@GetMapping("/home/{name}")
+	public String home(Model model, @PathVariable("name") String name)
 	{
-		List<Student> students = new ArrayList<>();		
-		students.add(new Student(1, "Ram", 90, "ram@gmail.com"));
-		students.add(new Student(2, "Sam", 75, "sam@gmail.com"));
-		students.add(new Student(3, "Tom", 80, "tom@gmail.com"));
-
-		model.addAttribute("students", students);
+		model.addAttribute("name",name);
 		return "home.html";
+	}
+	
+	@GetMapping("/about")
+	public String about()
+	{
+		return "about.html";
 	}
 
 }
